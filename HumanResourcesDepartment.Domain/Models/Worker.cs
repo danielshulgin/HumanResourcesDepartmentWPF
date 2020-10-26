bb3d22b9f58f710b5e808d60a97e41923fc4db14
Danil Shulgin <nik.vasilevich.01@gmail.com>
2020-10-26 14:34:05 +0200

    initial commit

---
+++ HumanResourcesDepartment.Domain/Models/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResourcesDepartment.Domain.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ HumanResourcesDepartment.EntityFramework/HumanResourcesDepartmentDBContext.cs
@@ -0,0 +1,21 @@
+﻿using HumanResourcesDepartment.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResourcesDepartment.EntityFramework
+{
+    public class HumanResourcesDepartmentDBContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=HumanResourcesDepartmentDB;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
